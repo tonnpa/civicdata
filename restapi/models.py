ab8185147c73850e97f9062ea480b95df143ed9a
Phan Anh Nguyen <tonnpa@gmail.com>
2017-05-18 16:16:05 -0400

    feat(data): added dataset information to the models
    Signed-off-by: Phan Anh Nguyen <tonnpa@gmail.com>

--- restapi/models.py
+++ restapi/models.py
@@ -6,3 +6,12 @@ class AtlantaTree(models.Model):
     medium_text = models.CharField(max_length=96, blank=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
+
+
+class Dataset(models.Model):
+    id = models.CharField(max_length=24, primary_key=True)
+    title = models.CharField(max_length=96)
+    collector = models.CharField(max_length=64)
+    date_from = models.CharField(max_length=16)
+    date_to = models.CharField(max_length=16)
+    description = models.TextField()
--- scripts/data_import.py
+++ scripts/data_import.py
@@ -21,3 +21,24 @@ def import_atlanta_trees():
             tree.save()
         except ValueError as ex:
             print(ex)
+
+    print('Importing Atlanta Trees has finished.')
+
+
+def import_dataset_information():
+    dataset_df = pd.read_excel('data/DatasetInformation.xlsx')
+    for record in dataset_df.itertuples():
+        try:
+            dataset = models.Dataset(
+                id=record.ID,
+                title=record.Title,
+                collector=record.Collector,
+                date_from=record.DateFrom,
+                date_to=record.DateTo,
+                description=record.Description,
+            )
+            dataset.save()
+        except ValueError as ex:
+            print(ex)
+
+    print('Importing Dataset Information has finished.')
