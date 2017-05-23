2e30e96dba4102c779f5853f4d9f28426dbd7b78
Phan Anh Nguyen <tonnpa@gmail.com>
2017-05-23 10:17:42 -0400

    feat(ui): tag placeholders, space between datasets
    Signed-off-by: Phan Anh Nguyen <tonnpa@gmail.com>

--- client/components/Dataset.js
+++ client/components/Dataset.js
@@ -1,43 +1,48 @@
 'use strict';
 
-import React from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
-import FaDownload from 'react-icons/lib/fa/download';
+import React from "react";
+import {Col, Grid, Row} from "react-bootstrap";
+import FaDownload from "react-icons/lib/fa/download";
 
-import Snapshot from './Snapshot';
-import Tag from './Tag';
+import Snapshot from "./Snapshot";
+import Tag from "./Tag";
 
 
 class Dataset extends React.Component {
-	render() {
-		const tags = [];
-		tags.push(<Tag key={0} bsStyle={'primary'} text={this.props.date}/>);
-		// tags.push(<Tag key={1} bsStyle={'success'} text={this.props.format}/>);
-		// tags.push(<Tag key={2} bsStyle={'warning'} text={this.props.size}/>);
+    TagList() {
+        return (
+            <ul className="list-inline">
+                <li><Tag key={0} bsStyle={'primary'} text="date"/></li>
+                <li><Tag key={1} bsStyle={'success'} text="format"/></li>
+                <li><Tag key={2} bsStyle={'warning'} text="size"/></li>
+            </ul>
+        );
+    }
 
-		return (
-			<Grid fluid={true}>
-				<Row>
-					<Col md={8}>
-                        <h2>{this.props.title}</h2>
-                        <hr/>
-						<span><b>Collector:</b> {this.props.collector}</span>
-						<p>{this.props.description}</p>
-						<p>{tags}</p>
-                        <a href={`/static/${this.props.file_name}`}
-                           download={this.props.file_name}>Download <FaDownload/></a>
-                        {/*<Preview hidden={this.props.hidePreview}*/}
-								 {/*onClick={this.props.onPreviewClick}/>*/}
-					</Col>
-					<Col md={4}>
-						<Snapshot image={this.props.image}
-								  visual_url={this.props.visual_url}
-								  visual_org={this.props.visual_org}/>
-					</Col>
-				</Row>
-			</Grid>
-		);
-	}
+    render() {
+        return (
+            <div className="dataset-container">
+                <Grid fluid={true}>
+                    <Row>
+                        <Col md={8}>
+                            <h2>{this.props.title}</h2>
+                            <hr/>
+                            <span><b>Collector:</b> {this.props.collector}</span>
+                            <p>{this.props.description}</p>
+                            {this.TagList()}
+                            <a href={`/static/${this.props.file_name}`}
+                               download={this.props.file_name}>Download <FaDownload/></a>
+                            {/*<Preview hidden={this.props.hidePreview}*/}
+                            {/*onClick={this.props.onPreviewClick}/>*/}
+                        </Col>
+                        <Col md={4}>
+                            <Snapshot image_name={this.props.file_name}/>
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
+        );
+    }
 }
 
 export default Dataset;
