847495ae8d89bbe535d23c1b5d4cfb225936f081
Phan Anh Nguyen <tonnpa@gmail.com>
2017-05-22 21:05:27 -0400

    chore(scripts): modify import function to include file name, format
    Signed-off-by: Phan Anh Nguyen <tonnpa@gmail.com>

--- scripts/data_import.py
+++ scripts/data_import.py
@@ -3,28 +3,6 @@ import pandas as pd
 from restapi import models
 
 
-def import_atlanta_trees():
-    trees_df = pd.read_excel('data/TreesAtlanta.xlsx')
-    for record in trees_df.itertuples():
-        if record[0] > 1000:
-            break
-
-        if record[0] % 100 == 0:
-            print('Imported {} records'.format(record[0]))
-        try:
-            tree = models.AtlantaTree(
-                long_text=record[1],
-                medium_text=record[2],
-                latitude=record.Latitude,
-                longitude=record.Longitude,
-            )
-            tree.save()
-        except ValueError as ex:
-            print(ex)
-
-    print('Importing Atlanta Trees has finished.')
-
-
 def import_dataset_information():
     dataset_df = pd.read_excel('data/DatasetInformation.xlsx')
     for record in dataset_df.itertuples():
@@ -36,6 +14,8 @@ def import_dataset_information():
                 date_from=record.DateFrom,
                 date_to=record.DateTo,
                 description=record.Description,
+                file_name=record.FileName,
+                format=record.Format,
             )
             dataset.save()
         except ValueError as ex:
