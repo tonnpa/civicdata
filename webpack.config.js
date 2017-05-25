cde7fa68124a547b155fbe306c1b33e6d387e1d3
Phan Anh Nguyen <tonnpa@gmail.com>
2017-05-25 16:58:42 -0400

    feat(ui): scss attempt
    Signed-off-by: Phan Anh Nguyen <tonnpa@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -4,48 +4,52 @@ const path = require('path');
 const BundleTracker = require('webpack-bundle-tracker');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/index.html',
-  filename: 'index.html',
-  inject: 'body'
+    template: './client/index.html',
+    filename: 'index.html',
+    inject: 'body'
 });
 
 module.exports = {
-  entry: './client/index.js',
-  output: {
-    path: path.resolve('assets'),
-    publicPath: '/static/',
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      { 
-        test: /\.(js|jsx)$/, 
-        use: 'babel-loader', 
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      { 
-        test: /\.(eot|svg|ttf|woff|woff2)$/, 
-        use: 'url-loader?limit=100000&name=[name].[ext]',
-      },
-      { 
-        test: /\.(png|jpg|ico)$/,
-        use: 'url-loader?limit=100000&name=[name].[ext]',
-      },
-    ]
-  },
-  plugins: [
-    HtmlWebpackPluginConfig,
-    new BundleTracker({filename: './webpack-stats.json'}),
+    entry: './client/index.js',
+    output: {
+        path: path.resolve('assets'),
+        publicPath: '/static/',
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'autoprefixer-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'autoprefixer-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: 'url-loader?limit=100000&name=[name].[ext]',
+            },
+            {
+                test: /\.(png|jpg|ico)$/,
+                use: 'url-loader?limit=100000&name=[name].[ext]',
+            },
+        ]
+    },
+    plugins: [
+        HtmlWebpackPluginConfig,
+        new BundleTracker({filename: './webpack-stats.json'}),
     ],
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    modules: ['node_modules'],
-  },
-  watchOptions: {
-    poll: true
-  }
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        modules: ['node_modules'],
+    },
+    watchOptions: {
+        poll: true
+    }
 }
