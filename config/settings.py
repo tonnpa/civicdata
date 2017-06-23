d337740dd4457a28ba78c0690b051e59208545cd
Phan Anh Nguyen <tonnpa@gmail.com>
2017-06-23 14:07:46 -0400

    conf(restapi): removed unused pagination configurations
    Signed-off-by: Phan Anh Nguyen <tonnpa@gmail.com>

--- config/settings.py
+++ config/settings.py
@@ -154,6 +154,4 @@ WEBPACK_LOADER = {
 # Django REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
 }
