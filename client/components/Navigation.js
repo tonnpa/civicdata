384a02b6f1b621e6eea93376cb0300304084759f
Phan Anh Nguyen <tonnpa@gmail.com>
2017-07-11 18:27:07 -0400

    fix(ui): removed padding for mobile view
    Signed-off-by: Phan Anh Nguyen <tonnpa@gmail.com>

--- client/components/Navigation.js
+++ client/components/Navigation.js
@@ -8,30 +8,32 @@ import {Nav, Navbar, NavDropdown, NavItem, MenuItem} from 'react-bootstrap'
 import SearchBar from './SearchBar'
 
 const Navigation = ({datasets = [], filterText, onSearchInputChange}) => (
-    <Navbar fixedTop fluid collapseOnSelect>
-        <Navbar.Header>
-            <Navbar.Brand>
-                <a href="#">CIVIC DATA</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-            <Navbar.Form pullLeft>
-                <SearchBar filterText={filterText}
-                           onSearchInputChange={onSearchInputChange}/>
-            </Navbar.Form>
-            <Nav pullRight>
-                <NavDropdown eventKey={1} title="Datasets" id="datasets-dropdown">
-                    {datasets.map(dataset =>
-                        <MenuItem eventKey={dataset.id} key={dataset.id}
-                                  href={`#${dataset.id}`}>
-                            {dataset.title}
-                        </MenuItem>
-                    )}
-                </NavDropdown>
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
+    <header>
+        <Navbar fixedTop fluid collapseOnSelect>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <a href="#">CIVIC DATA</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+                <Navbar.Form pullLeft>
+                    <SearchBar filterText={filterText}
+                               onSearchInputChange={onSearchInputChange}/>
+                </Navbar.Form>
+                <Nav pullRight>
+                    <NavDropdown eventKey={1} title="Datasets" id="datasets-dropdown">
+                        {datasets.map(dataset =>
+                            <MenuItem eventKey={dataset.id} key={dataset.id}
+                                      href={`#${dataset.id}`}>
+                                {dataset.title}
+                            </MenuItem>
+                        )}
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    </header>
 )
 
 export default Navigation
