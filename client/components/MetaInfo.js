d293f2297d7142bf8aec126502376e8b9bf98033
Phan Anh Nguyen <tonnpa@gmail.com>
2017-07-14 19:04:55 -0400

    feat(ui): tooltip for explaining some column names
    Signed-off-by: Phan Anh Nguyen <tonnpa@gmail.com>

--- client/components/MetaInfo.js
+++ client/components/MetaInfo.js
@@ -4,13 +4,31 @@
 'use strict'
 
 import React from 'react'
+import {OverlayTrigger, Tooltip} from 'react-bootstrap'
+
+const Feature = ({record}) => {
+    if (record.comment) {
+        const tooltip = (
+            <Tooltip id={record.feature}>{record.comment}</Tooltip>
+        )
+        return (
+            <OverlayTrigger placement="bottom" overlay={tooltip}>
+                <dt className="feature-comment">{record.feature} <span className="fa fa-question-circle-o"
+                                                                       aria-hidden="true"></span>
+                </dt>
+            </OverlayTrigger>
+        )
+    }
+    return (<dt>{record.feature}</dt>)
+}
 
 const MetaInfo = ({content = []}) => (
     <div>
         <dl className="dl-horizontal">
             {content.map(record => (
                 <div key={record.feature} className="feature-info">
-                    <dt>{record.feature}</dt>
+
+                    <Feature record={record}/>
                     <dd>{record.description}</dd>
                 </div>
             ))}
