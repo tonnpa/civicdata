e1579df47351a5bbeea346f5a340d7d147a06cb1
Phan Anh Nguyen <tonnpa@gmail.com>
2017-09-22 12:31:42 -0400

    fix(db): allow longer dataset identifiers
    Signed-off-by: Phan Anh Nguyen <tonnpa@gmail.com>

--- restapi/models.py
+++ restapi/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Dataset(models.Model):
-    id = models.CharField(max_length=24, primary_key=True)
+    id = models.CharField(max_length=48, primary_key=True)
     title = models.CharField(max_length=96)
     collector = models.CharField(max_length=64)
     date_from = models.CharField(max_length=16)
