db84368952bf1f838ce69ef6cfedf708fffaf08e
Phan Anh Nguyen <tonnpa@gmail.com>
2017-09-25 14:14:32 -0400

    feat(seo): alternative title for images
    Signed-off-by: Phan Anh Nguyen <tonnpa@gmail.com>

--- client/components/Snapshot.js
+++ client/components/Snapshot.js
@@ -8,6 +8,7 @@ const Snapshot = ({image_file_name, title, caption, tab, isOpen, onToggle}) => (
     <div>
         <Image src={`/static/media/data-visualizations/${image_file_name}`}
                className="center-block"
+               alt={`Dataset Snapshot of ${title}`}
                onClick={onToggle}
                style={{display: (tab === TabTitles.PREVIEW) ? "none" : "inline"}}
                thumbnail responsive/>
