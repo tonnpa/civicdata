f8a2e890da2578ce34baceb5d153d1fd1e9f1fb1
Phan Anh Nguyen <tonnpa@gmail.com>
2017-10-12 11:00:12 -0400

    fix(restapi): url paths
    Signed-off-by: Phan Anh Nguyen <tonnpa@gmail.com>

--- restapi/urls.py
+++ restapi/urls.py
@@ -3,9 +3,9 @@ from rest_framework.routers import DefaultRouter
 from restapi import views
 
 router = DefaultRouter()
-router.register(r'^api/datainfo', views.DatasetViewSet)
-router.register(r'^api/datafiles', views.DataFileViewSet)
-router.register(r'^api/metainfo', views.MetaInfoViewSet)
+router.register(r'api/datainfo', views.DatasetViewSet)
+router.register(r'api/datafiles', views.DataFileViewSet)
+router.register(r'api/metainfo', views.MetaInfoViewSet)
 
 urlpatterns = router.urls
 
