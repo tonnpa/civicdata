ece3e312a79c325cd334c22568a599726c0c1104
Phan Anh Nguyen <tonnpa@gmail.com>
2017-10-12 11:27:10 -0400

    fix(ui): navbar left and right padding
    Signed-off-by: Phan Anh Nguyen <tonnpa@gmail.com>

--- client/components/Navigation.js
+++ client/components/Navigation.js
@@ -4,41 +4,43 @@
 'use strict'
 
 import React from 'react'
-import {Nav, Navbar, NavDropdown, NavItem, MenuItem} from 'react-bootstrap'
+import {Grid, Nav, Navbar, NavDropdown, NavItem, MenuItem} from 'react-bootstrap'
 import {scroller} from 'react-scroll'
 import SearchBar from './SearchBar'
 
 const Navigation = ({datasets = [], filterText, onSearchInputChange}) => (
     <header>
-        <Navbar fixedTop fluid collapseOnSelect>
-            <Navbar.Header>
-                <Navbar.Brand>
-                    <a href="#"><h1>CIVIC DATA</h1></a>
-                </Navbar.Brand>
-                <Navbar.Toggle />
-            </Navbar.Header>
-            <Navbar.Collapse>
-                <Navbar.Form pullLeft>
-                    <SearchBar filterText={filterText}
-                               onSearchInputChange={onSearchInputChange}/>
-                </Navbar.Form>
-                <Navbar.Text pullRight>
-                    <Navbar.Link href="/register">
-                        Register
-                    </Navbar.Link>
-                </Navbar.Text>
-                <Nav pullRight>
-                    <NavDropdown eventKey={1} title="Datasets" id="datasets-dropdown">
-                        {datasets.map(dataset =>
-                            <MenuItem eventKey={dataset.id} key={dataset.id}
-                                      onSelect={() => scroller.scrollTo(dataset.id, {smooth: "true"})}>
-                                {dataset.title}
-                            </MenuItem>
-                        )}
-                    </NavDropdown>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <Grid>
+            <Navbar fixedTop collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#"><h1>CIVIC DATA</h1></a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Navbar.Form pullLeft>
+                        <SearchBar filterText={filterText}
+                                   onSearchInputChange={onSearchInputChange}/>
+                    </Navbar.Form>
+                    <Navbar.Text pullRight>
+                        <Navbar.Link href="/register">
+                            Register
+                        </Navbar.Link>
+                    </Navbar.Text>
+                    <Nav pullRight>
+                        <NavDropdown eventKey={1} title="Datasets" id="datasets-dropdown">
+                            {datasets.map(dataset =>
+                                <MenuItem eventKey={dataset.id} key={dataset.id}
+                                          onSelect={() => scroller.scrollTo(dataset.id, {smooth: "true"})}>
+                                    {dataset.title}
+                                </MenuItem>
+                            )}
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </Grid>
     </header>
 )
 
